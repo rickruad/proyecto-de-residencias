f326d1f88457ac88f4293e33fe965dd52b8477a4
Syoker <lautarivan@hotmail.com>
2023-03-29 11:31:29 -0300

    added models

--- client/src/pages/products/index.tsx
+++ client/src/pages/products/index.tsx
@@ -3,13 +3,26 @@ import Server from '@/hooks/Server';
 import Head from '@/components/Head';
 import Header from '@/components/Header';
 
+import styles from '@/styles/products.module.css';
+
 export default function Home() {
+  const cardHTML = [];
 
   Server.useLoginAuthenticationInsidePage();
+
+  for (let i = 0; i < 10; i++) {
+    cardHTML.push(
+      <div key={i} className={styles.card}></div>
+    )
+  }
 
   return <>
     <Head title='Productos' />
 
     <Header />
+
+    <section className={styles.container}>
+      {cardHTML}
+    </section>
   </>
 }

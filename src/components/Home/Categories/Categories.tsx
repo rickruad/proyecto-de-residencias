02ae9251e2c15c60b423f3af67d81e4ab923a30b
Syoker <lautarivan@hotmail.com>
2023-05-27 22:30:45 -0300

    some changes

--- src/components/Home/Categories/Categories.tsx
+++ src/components/Home/Categories/Categories.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
-import { MdShoppingBag, MdOutlineAddCircle } from 'react-icons/md';
+import { MdShoppingBag } from 'react-icons/md';
 
 import * as StringUtilities from 'src/utils/StringUtilities';
 
--- src/components/Home/Services/Services.tsx
+++ src/components/Home/Services/Services.tsx
@@ -1,18 +1,72 @@
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 import { MdWorkspacesFilled } from 'react-icons/md';
+
+import * as StringUtilities from 'src/utils/StringUtilities';
+
+import Image from 'next/image';
 
 import styles from './styles/services.module.css';
 
-export default function Services() {
-  return <>
-    <section className={styles.servicesSection}>
-      <div className={styles.servicesContainer}>
-        <div className={styles.titleSection}>
-          <MdWorkspacesFilled className={styles.icon} />
-          <h2>{'Servicios'}</h2>
-          <div className={styles.line} />
-        </div>
-        <h3>{'Disfruta de los servicios que te ofrecemos'}</h3>
-      </div>
-    </section>
-  </>
+interface SessionProps {
+	session: string;
+}
+
+export default function Services(props: SessionProps) {
+	const { session } = props;
+	const router = useRouter();
+
+	const servicesName = ['cfe', 'telmex', 'otros'];
+
+	const services = servicesName.map((service, index) => {
+		return {
+			id: index,
+			name: service,
+			image: `/img/services/${service}.png`,
+		};
+	});
+
+	const renderServices = services.map((currentService) => {
+		const service = currentService.name;
+		const name =
+			currentService.name === 'cfe'
+				? currentService.name.toUpperCase()
+				: StringUtilities.wordsToCapitalLetter({ text: currentService.name });
+
+		const routerPush = () => {
+			if (session) {
+				router.push({ pathname: './service', query: { service } });
+			}
+		};
+
+		return (
+			<button onClick={routerPush} key={currentService.id} className={styles.category}>
+				<Image
+					className={styles.image}
+					src={currentService.image}
+					alt={currentService.name}
+					width={200}
+					height={200}
+					priority={true}
+				/>
+				<h3 className={styles.title}>{name}</h3>
+			</button>
+		);
+	});
+
+	return (
+		<>
+			<section className={styles.servicesSection}>
+				<div className={styles.servicesContainer}>
+					<div className={styles.titleSection}>
+						<MdWorkspacesFilled className={styles.icon} />
+						<h2>{'Servicios'}</h2>
+						<div className={styles.line} />
+					</div>
+					<h3>{'Disfruta de los servicios que te ofrecemos'}</h3>
+					<article className={styles.renderServices}>{renderServices}</article>
+				</div>
+			</section>
+		</>
+	);
 }
